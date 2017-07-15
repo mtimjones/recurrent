e28dcb9ab27b7cdd1e932513322351b879b45036
M. Tim Jones <mtj@mtjones.com>
2017-07-15 13:23:12 -0600

    Added ga.c

---
+++ ga.c
@@ -0,0 +1,36 @@
+// ga.c
+
+#include "nettypes.h"
+#include "ga.h"
+
+static Member population[ 2 ][ POP_SIZE ];
+
+#define getRandWeight( )  ( getRand( 65536 ) )
+
+
+void GA_init_population( unsigned int pop )
+{
+   int i, j;
+
+   for ( i = 0 ; i < POP_SIZE ; i++ )
+   {
+      for ( j = 0 ; j < NUM_WEIGHTS ; j++ )
+      {
+         population[ pop ][ i ].chromosome[ j ] = getRandWeight( );
+      }
+      population[ pop ][ i ].fitness = 0.0;
+   }
+
+   return;
+}
+
+
+double GA_get_member_weight( unsigned int pop, unsigned int member, unsigned int index )
+{
+   // Weights are 0..65535.
+   // Translated as ( Weight - 32767 ) * 0.001
+
+   return ( ( double )population[ pop ][ member ].chromosome[ index ] - 32767.0 ) * 0.001;
+}
+
+
